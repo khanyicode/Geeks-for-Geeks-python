a09097072871c270918e8c73a26abe78487fa857
fmabaso023 <khanyisilemesha@gmail.com>
2024-03-22 14:37:59 +0200

    first attempt

---
+++ python_strings/strings.py
@@ -0,0 +1,18 @@
+
+def palindrom():
+    word = "racecar"
+    if word == word[::-1]:
+        print(word,"is palindrom")
+        
+    else:
+        print(word," is not plaindrom")
+    
+palindrom()
+
+
+
+    
+
+    
+    
+    
